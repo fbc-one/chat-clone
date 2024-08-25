41397688ee4858748da1cfea3f8706065ce0f8f1
fbc-one <53253919+fbc-one@users.noreply.github.com>
2024-08-25 22:28:58 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,9 +11,3 @@ def get_chat_response(prompt, memory, openai_api_key):
 
     response = chain.invoke({"input": prompt})
     return response["response"]
-
-
-# memory = ConversationBufferMemory(return_messages=True)
-# , os.getenv("OPENAI_API_KEY"
-# print(get_chat_response("牛顿提出过哪些知名的定律？", memory, "sk-yJ4RfM15owQIX1jTgmuTgxpiHUSSKpB5ZWvOoNYrx4xc7tOn"))
-# print(get_chat_response("我上一个问题是什么？", memory, "sk-yJ4RfM15owQIX1jTgmuTgxpiHUSSKpB5ZWvOoNYrx4xc7tOn"))
